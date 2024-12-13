59935cb83047854923a56735d04d12081d72266b
amineelmechrafi <aminemechrafiaminne@gmail.com>
2024-12-13 21:02:38 +0000

    adding prime ng and tailwind css

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { ButtonModule } from 'primeng/button';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,ButtonModule , CommonModule ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient()
-  ]
+    providers: [
+        provideAnimationsAsync(),
+        providePrimeNG({ 
+            theme: {
+                preset: Aura
+            }
+        })
+    ]
 };
