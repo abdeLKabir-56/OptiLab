88719259c44d7fcc7f9d5cc8896590f3b99e41eb
amineelmechrafi <aminemechrafiaminne@gmail.com>
2024-12-17 21:55:51 +0000

    setting up tailwind css and preline ui

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Router, Event, NavigationEnd } from '@angular/router';
+
+import { IStaticMethods } from 'preline/preline';
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -10,4 +18,17 @@ import { RouterModule } from '@angular/router';
 })
 export class AppComponent {
   title = 'OptiLab';
+  constructor(private router: Router) {
+  
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          window.HSStaticMethods.autoInit();
+        }, 100);
+      }
+    });
+  }
 }
