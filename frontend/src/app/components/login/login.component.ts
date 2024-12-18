468ededeb4081b9f7b4096b32e68f13458895695
amineelmechrafi <aminemechrafiaminne@gmail.com>
2024-12-18 12:02:57 +0000

    adding login component

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  @Input() isOpen!:boolean
+  @Output() closeModal=new EventEmitter()
+
+  onClose(){
+    this.closeModal.emit()
+  }
+
+}
--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,16 +1,20 @@
+import { LoginComponent } from './../../components/login/login.component';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
+  isOpenModal=false
   constructor(private router: Router) {}
-
+ toggleModal(){
+  this.isOpenModal=!this.isOpenModal
+ }
   goToDashboard(): void {
     this.router.navigate(['/dashboard']);
   }
