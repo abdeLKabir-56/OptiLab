f701a1163f442ba10e1cd207941c451e805a7a36
amineelmechrafi <aminemechrafiaminne@gmail.com>
2024-12-20 15:01:26 +0000

    setting the layout for the dashboard component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,11 +5,26 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LaboratoireComponent } from './laboratoire/laboratoire/laboratoire.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { MainDashboardComponent } from './pages/main-dashboard/main-dashboard.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: LandingComponent, title: 'OptiLab - Home' },
-  { path: 'laboratoires', component: LaboratoireComponent, title: 'Laboratories Management' },
-  { path: 'dashboard', component: DashboardComponent , canActivate: [AuthGuard]},
-  
+  {
+    path: '',
+    component: AppComponent, // Main layout
+    children: [
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', component: LandingComponent, title: 'OptiLab - Home' },
+      { path: 'laboratoires', component: LaboratoireComponent, title: 'Laboratories Management' },
+    ]
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent, // Separate layout
+    canActivate: [AuthGuard],
+    title: 'Dashboard',
+    children: [
+      { path: '', component: MainDashboardComponent, title: 'OptiLab - Home' },
+    ]
+  },
+  { path: '**', redirectTo: '/home' }, // Fallback for unknown routes
 ];
--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SiderComponent } from '../../components/sider/sider.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,HeaderComponent,SiderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
