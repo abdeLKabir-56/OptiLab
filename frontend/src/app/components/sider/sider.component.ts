b7d0e7fcfadb6f2eca0ee6dbd5e15f44b7c158d4
amineelmechrafi <aminemechrafiaminne@gmail.com>
2024-12-21 08:35:37 +0000

    completing dashboard component

--- frontend/src/app/components/sider/sider.component.ts
+++ frontend/src/app/components/sider/sider.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import {ChangeDetectionStrategy} from '@angular/core';
-import {TuiButton} from '@taiga-ui/core';
  
 
 
 
 @Component({
     selector: 'app-sider',
-    imports: [TuiButton],
+    imports: [],
     templateUrl: './sider.component.html',
     styleUrl: './sider.component.css', 
     changeDetection: ChangeDetectionStrategy.OnPush,
