782de7a8788a6a0ee59871ce3056dd75d14189bc
amineelmechrafi <aminemechrafiaminne@gmail.com>
2025-01-05 23:01:46 +0000

    fixing client work´flow 3

--- frontend/setup-jest.ts
+++ frontend/setup-jest.ts
@@ -1,4 +1,5 @@
 import 'jest-preset-angular/setup-jest';
+
 jest.mock('@primeng/themes/aura', () => {
   return {}; // Mock the Aura theme module to avoid import errors
-});
+}, { virtual: true }); // Us
