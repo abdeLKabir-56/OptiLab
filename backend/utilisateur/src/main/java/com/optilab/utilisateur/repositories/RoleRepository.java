cac831e8c2b07e9c78209743887f2bdaa778c58e
amineelmechrafi <aminemechrafiaminne@gmail.com>
2025-01-05 22:40:08 +0000

    testing frontendend workflow

--- backend/utilisateur/src/main/java/com/optilab/utilisateur/repositories/RoleRepository.java
+++ backend/utilisateur/src/main/java/com/optilab/utilisateur/repositories/RoleRepository.java
@@ -3,9 +3,11 @@ package com.optilab.utilisateur.repositories;
 import com.optilab.utilisateur.entities.Role;
 import com.optilab.utilisateur.enums.RoleUtilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByNom(RoleUtilisateur name);
     Boolean existsByNom(RoleUtilisateur roleUtilisateur);
--- backend/utilisateur/src/main/java/com/optilab/utilisateur/repositories/UtilisateurRepository.java
+++ backend/utilisateur/src/main/java/com/optilab/utilisateur/repositories/UtilisateurRepository.java
@@ -3,9 +3,11 @@ package com.optilab.utilisateur.repositories;
 import com.optilab.utilisateur.entities.Utilisateur;
 import com.optilab.utilisateur.enums.RoleUtilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
     List<Utilisateur> findByRoleNom(RoleUtilisateur roleName);
     Boolean existsByEmailOrUsername(String email, String username);
